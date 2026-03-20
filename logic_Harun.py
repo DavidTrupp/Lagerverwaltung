ed8327a86d7bf1013af7ae8d49a3106196ef64a1
HarunSmriko <harun.smriko10@gmail.com>
2026-03-20 19:50:53 +0100

    neue Funktionen + Änderungen in logic_Harun.py

--- logic_Harun.py
+++ logic_Harun.py
@@ -2,6 +2,7 @@
 Datum: 17.03.2026
 Kurzbeschreibung: Enthält die Logikfunktionen für die Lagerverwaltung.
 """
+from efesdevelope import article_exists, find_product_by_id   #importiert die Funktion article_exists und find_product_by_id aus der Datei efesdevelope.py, um sie in diesem Skript verwenden zu können
 
 def search_products_by_name(products, search_term):
     """Sucht Produkte anhand eines Suchbegriffs im Namen.
@@ -20,6 +21,7 @@ def search_products_by_name(products, search_term):
             found_products.append(product)          #wird der neuen Liste hinzugefügt
     return found_products                           #gibt die neue Liste zurück
 
+
 def increase_stock(products, product_id, amount):
     """Erhöht den Lagerbestand eines Produkts."""
     for product in products:                        #durchläuft die Liste der Produkte komplett
@@ -28,15 +30,17 @@ def increase_stock(products, product_id, amount):
             return True                             #gibt True zurück, wenn die Erhöhung erfolgreich war
     return False                                    #gibt False zurück, wenn kein Produkt mit der angegebenen ID gefunden wurde
 
+
 def decrease_stock(products, product_id, amount):
     """Verringert den Lagerbestand eines Produkts."""
-    product = find_product(products, article_id)              #sucht das Produkt mit der angegebenen ID
+    product = find_product_by_id(products, product_id)              #sucht das Produkt mit der angegebenen ID
     if product == None:
         return False, "Fehler: Artikel nicht gefunden"        #gibt Fehlermeldung zurück, wenn kein Produkt mit der angegebenen ID gefunden wurde
     if product['stock'] < amount:
         return False, "Fehler: Nicht genügend Lagerbestand"   #gibt Fehlermeldung zurück, wenn nicht genügend Lagerbestand vorhanden ist
     product['stock'] -= amount                                #wird der Lagerbestand um die angegebene Menge verringert
     return True, "Lagerbestand erfolgreich verringert. Neuer Bestand: {}".format(product['stock'])        #gibt Erfolgsmeldung mit dem neuen Bestand zurück
+
 
 def get_low_stock_products(products):
     """Gibt alle Artikel zurück, die den Mindestbestand erreicht
@@ -46,3 +50,15 @@ def get_low_stock_products(products):
         if product['stock'] <= product['min_stock']:    #wenn der Lagerbestand kleiner oder gleich dem Mindestbestand ist
             low_stock_products.append(product)          #wird das Produkt zur Liste der Artikel mit niedrigem Lagerbestand hinzugefügt
     return low_stock_products                           #gibt die Liste der Artikel mit niedrigem Lagerbestand zurück
+
+
+def get_statistics(products):
+    """Gibt Statistiken über die Produkte zurück."""
+    total_products = len(products)                                  #berechnet die Gesamtzahl der Produkte
+    total_stock = sum(product['stock'] for product in products)     #berechnet die Gesamtmenge aller Lagerbestände
+    low_stock_count = len(get_low_stock_products(products))         #berechnet die Anzahl der Artikel mit niedrigem Lagerbestand
+    return {
+        'total_products': total_products,
+        'total_stock': total_stock,
+        'low_stock_count': low_stock_count
+    }
